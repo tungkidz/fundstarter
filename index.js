b24f54ced108e1181a6471dc16f0ec4bba23c893
Tung Tran <tung.tran@sjsu.edu>
2016-10-01 21:30:05 +0000

    using readFile

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-var express = require('express')
-var app = express()
+var http = require('http');
+var fs = require('fs');
+var port = 8080;
 
+var server = http.createServer(function(request, response){
+    fs.readFile("public/index.html", function(err, data){
+	if(err){
+	    console.log("error:", err);
+	    }
+	else{
+	    response.writeHead(200, {"Content-Type":"text/html"});
+	    response.write(data);
+	    response.end();
+	    }
+	})
+});
 
-app.set('port', (process.env.PORT || 8080))
-//app.use(express.static(__dirname + '/public'))
-
-//__dirname returns the directory that the currently executing script is in.
-
-app.get('/', function(request, response) {
-    response.sendFile('public/index.html',{root:__dirname})
-
-/* sends an entire HTTP response to the client,                                                                                                                                     
- including headers and content,                                                                                                                                                     
- which is why you can only call once*/
-
-
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at :" + app.get('port'))
-})
+server.listen(port, function(){
+    console.log("Node app running @port", port);
+});
